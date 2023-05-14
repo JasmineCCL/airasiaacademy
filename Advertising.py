a5bfe206a6adf4709e4d2ee6320df3cdb7923f38
JasmineCCL <133468147+JasmineCCL@users.noreply.github.com>
2023-05-14 11:22:33 +0800

    Update Advertising.py

--- Advertising.py
+++ Advertising.py
@@ -25,7 +25,7 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-loaded_model = pickle.load(open("Advertising.py", "rb"))
+loaded_model = pickle.load(open("Sales.h5", "rb"))
 
 prediction = loaded_model.predict(df)
 
