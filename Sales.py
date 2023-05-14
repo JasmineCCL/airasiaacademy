77ffa4e324537768b8db859d212d62a18887ea41
JasmineCCL <133468147+JasmineCCL@users.noreply.github.com>
2023-05-14 12:43:11 +0800

    Update Sales.py

--- Sales.py
+++ Sales.py
@@ -11,9 +11,9 @@ This app predicts the **Advertising** sales!
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    TV = st.sidebar.slider('Number of TV', 0, 200, 100)
+    TV = st.sidebar.slider('Number of TV', 0, 100, 100)
     Radio = st.sidebar.slider('Number of Radio',0, 300, 10)
-    Newspaper = st.sidebar.slider('Number of Newspaper', 0, 100, 50)
+    Newspaper = st.sidebar.slider('Number of Newspaper', 0, 200, 50)
     data = {'TV': TV,
             'Radio': Radio,
             'Newspaper': Newspaper}
